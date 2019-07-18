b256f54e3e107f91a80a93d195354bf94e827e12
Marc Lytle <mlytle4218@gmail.com>
2019-07-18 13:01:53 -0700

    workign through the acoustid results

--- start.py
+++ start.py
@@ -2,7 +2,10 @@
 import sys, os
 import eyed3
 import requests
-
+import math
+import json
+import acoustid
+import pprint
 # files = os.listdir(sys.argv[1])
 
 genres = set()
@@ -16,18 +19,38 @@ genres = set()
 # for genre in genres:
 #     print(genre)
 
-import acoustid
-import chromaprint
-duration, fp_encoded = acoustid.fingerprint_file(sys.argv[1])
-fingerprint, version = chromaprint.decode_fingerprint(fp_encoded)
-# key = 'd6nRzqa53O'
-# key = 'UoWqZj5SUA'
-key = 'hRAJuFbOWAk'
-# print(fp_encoded.decode())
 
-url = 'https://api.acoustid.org/v2/lookup?client={}&duration=641&fingerprint={}'.format(key,fp_encoded.decode())
-# url ='https://api.acoustid.org/v2/lookup?client={}&duration=641&fingerprint=AQABz0qUkZK4oOfhL-CPc4e5C_wW2H2QH9uDL4cvoT8UNQ-eHtsE8cceeFJx-LiiHT-aPzhxoc-Opj_eI5d2hOFyMJRzfDk-QSsu7fBxqZDMHcfxPfDIoPWxv9C1o3yg44d_3Df2GJaUQeeR-cb2HfaPNsdxHj2PJnpwPMN3aPcEMzd-_MeB_Ej4D_CLP8ghHjkJv_jh_UDuQ8xnILwunPg6hF2R8HgzvLhxHVYP_ziJX0eKPnIE1UePMByDJyg7wz_6yELsB8n4oDmDa0Gv40hf6D3CE3_wH6HFaxCPUD9-hNeF5MfWEP3SCGym4-SxnXiGs0mRjEXD6fgl4LmKWrSChzzC33ge9PB3otyJMk-IVC6R8MTNwD9qKQ_CC8kPv4THzEGZS8GPI3x0iGVUxC1hRSizC5VzoamYDi-uR7iKPhGSI82PkiWeB_eHijvsaIWfBCWH5AjjCfVxZ1TQ3CvCTclGnEMfHbnZFA8pjD6KXwd__Cn-Y8e_I9cq6CR-4S9KLXqQcsxxoWh3eMxiHI6TIzyPv0M43YHz4yte-Cv-4D16Hv9F9C9SPUdyGtZRHV-OHEeeGD--BKcjVLOK_NCDXMfx44dzHEiOZ0Z44Rf6DH5R3uiPj4d_PKolJNyRJzyu4_CTD2WOvzjKH9GPb4cUP1Av9EuQd8fGCFee4JlRHi18xQh96NLxkCgfWFKOH6WGeoe4I3za4c5hTscTPEZTES1x8kE-9MQPjT8a8gh5fPgQZtqCFj9MDvp6fDx6NCd07bjx7MLR9AhtnFnQ70GjOcV0opmm4zpY3SOa7HiwdTtyHa6NC4e-HN-OfC5-OP_gLe2QDxfUCz_0w9l65HiPAz9-IaGOUA7-4MZ5CWFOlIfe4yUa6AiZGxf6w0fFxsjTOdC6Itbh4mGD63iPH9-RFy909XAMj7mC5_BvlDyO6kGTZKJxHUd4NDwuZUffw_5RMsde5CWkJAgXnDReNEaP6DTOQ65yaD88HoeX8fge-DSeHo9Qa8cTHc80I-_RoHxx_UHeBxrJw62Q34Kd7MEfpCcu6BLeB1ePw6OO4sOF_sHhmB504WWDZiEu8sKPpkcfCT9xfej0o0lr4T5yNJeOvjmu40w-TDmqHXmYgfFhFy_M7tD1o0cO_B2ms2j-ACEEQgQgAIwzTgAGmBIKIImNQAABwgQATAlhDGCCEIGIIM4BaBgwQBogEBIOESEIA8ARI5xAhxEFmAGAMCKAURKQQpQzRAAkCCBQEAKkQYIYIQQxCixCDADCABMAE0gpJIgyxhEDiCKCCIGAEIgJIQByAhFgGACCACMRQEyBAoxQiHiCBCFOECQFAIgAABR2QAgFjCDMA0AUMIoAIMChQghChASGEGeYEAIAIhgBSErnJPPEGWYAMgw05AhiiGHiBBBGGSCQcQgwRYJwhDDhgCSCSSEIQYwILoyAjAIigBFEUQK8gAYAQ5BCAAjkjCCAEEMZAUQAZQCjCCkpCgFMCCiIcVIAZZgilAQAiSHQECOcQAQIc4QClAHAjDDGkAGAMUoBgyhihgEChFCAAWEIEYwIJYwViAAlHCBIGEIEAEIQAoBwwgwiEBAEEEOoEwBY4wRwxAhBgAcKAESIQAwwIowRFhoBhAE'.format(key)
-print(url)
-result = requests.get(url)
-print(result.content)
-# print(fingerprint)
+my_file = sys.argv[1]
+duration, fp_encoded = acoustid.fingerprint_file(my_file)
+key = 'd6nRzqa53O'
+
+audiofile = eyed3.load(sys.argv[1])
+
+duration = math.floor(audiofile.info.time_secs)
+
+#url = 'https://api.acoustid.org/v2/lookup?client={}&meta=recordings+recordingids+releases+releaseids+releasegroups+releasegroupids+tracks+compress+usermeta+sources&duration={}&fingerprint={}'.format(key,duration,fp_encoded.decode())
+url = 'https://api.acoustid.org/v2/lookup?client={}&meta=recordings+sources+compress&duration={}&fingerprint={}'.format(key,duration,fp_encoded.decode())
+
+results = requests.get(url)
+json_result = json.loads(results.content.decode())
+pp = pprint.PrettyPrinter(indent=4)
+score = {'value':0,'itr':0}
+
+for itr,result in enumerate(json_result['results']):
+    # print(result['score'])
+    if result['score'] > score['value']:
+        score['value'] =  result['score']
+        score['itr'] = itr
+
+    # print('*********************************')
+    # for recording in result['recordings']:
+    #     pp.pprint(recording)
+    #     print()
+
+sources ={'value':0,'itr':0}
+for itr,recording in enumerate(json_result['results'][score['itr']]['recordings']):
+    if recording['sources'] > sources['value']:
+        sources['value'] = recording['sources']
+        sources['itr'] = itr
+
+print(json_result['results'][score['itr']]['recordings'][sources['itr']])
